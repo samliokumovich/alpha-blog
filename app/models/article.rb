1466792afe9c782727835075ce0fd1f43cbd4e9b
Samuel Liokumovich <191liokumovichs@student.dist113.org>
2016-08-07 16:16:31 -0500

    styling

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+
+  #validatoions!!!!
+  validates :title, presence: true, length: { minimum: 3 }
+  validates :description, presence: true, length: { minimum: 16 }
 end
